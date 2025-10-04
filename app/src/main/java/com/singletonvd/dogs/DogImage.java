bf8e7ecee912e10da33c835eaf771460de7d23a9
SingletonVD <singletonvd@mail.ru>
2025-10-04 14:52:29 +0300

    feat: add simple server response reading using no external libraries

---
+++ app/src/main/java/com/singletonvd/dogs/DogImage.java
@@ -0,0 +1,12 @@
+package com.singletonvd.dogs;
+
+public class DogImage {
+
+    private String image;
+    private String status;
+
+    public DogImage(String image, String status) {
+        this.image = image;
+        this.status = status;
+    }
+}
--- app/src/main/java/com/singletonvd/dogs/MainActivity.java
+++ app/src/main/java/com/singletonvd/dogs/MainActivity.java
@@ -1,24 +1,42 @@
 package com.singletonvd.dogs;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String BASE_URL = "https://dog.ceo/api/breeds/image/random";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        loadImage();
+    }
+
+    private void loadImage() {
+        new Thread(() -> {
+            try {
+
+                URL url = new URL(BASE_URL);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = urlConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String result = bufferedReader.readLine();
+                Log.d("MainActivity", result);
+            } catch (Exception e) {
+                Log.d("MainActivity", e.toString());
+            }
+        }).start();
     }
 }
